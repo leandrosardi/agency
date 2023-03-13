3a0e07f6107998feafd792056cb9e883022e129b
leandrosardi <leandro.sardi@expandedventure.com>
2023-03-13 17:24:09 -0300

    setup agency extnesion

--- extension.rb
+++ extension.rb
@@ -1,22 +1,23 @@
 BlackStack::Extensions::add ({
     # descriptive name and descriptor
-    :name => '<!your-github-repo-name-for-this-extension!>',
-    
+    :name => 'agency',
+    :large_name => 'Warm Leads Program',
+
     # Write a description here. It will be shown in the main dashboard of your MySaaS project.
-    :description => '<!extension-description-here!>',
+    :description => 'Email Campanigns Driven by Our Experts, and Full Managed Configurations.<br/>From 10 to 15 Positive Replies Per Month, Guaranteed.<br/>Weekly Follow-Ups to Interested Leads.',
 
     # setup the url of the repository for installation and updates
-    :repo_url => 'https://github.com/<!your-github-username!>/<!your-github-repo-name-for-this-extension!>',
+    :repo_url => 'https://github.com/leandrosardi/agency',
     :repo_branch => 'main',
 
     # define version with format <mayor>.<minor>.<revision>
     :version => '0.0.1',
 
     # define the name of the author
-    :author => '<!your-github-username!>',
+    :author => 'Leandro Daniel Sardi',
 
     # what is the section to add this extension in either the top-bar, the footer, the dashboard.
-    :services_section => 'Education',
+    :services_section => 'Services',
     # show this extension as a service in the top bar?
     :show_in_top_bar => true,
     # show this extension as a service in the footer?
@@ -30,11 +31,9 @@ BlackStack::Extensions::add ({
 
     # what are the screens to add in the leftbar
     :leftbar_icons => [
-        { :label => 'results', :icon => :search, :screen => :results, },
-        { :label => 'exports', :icon => :'cloud-download', :screen => :exports, },
-        { :label => 'searches', :icon => :'save', :screen => :'saved_searches', },
+        { :label => 'dashboard', :icon => :chart, :screen => :dashboard, },
     ],
- 
+=begin
     # add a folder to the storage from where user can download the exports.
     :storage_folders => [
         { :name => 'leads.exports', },
@@ -75,15 +74,16 @@ BlackStack::Extensions::add ({
             :background => true,
         }],
     }],
-
+=end
 })
 
+=begin
 # setup the I2P product description here
 BlackStack::I2P::add_services([
     { 
-        :code=>'ces', 
-        :name=>'Cold Emails Seminar', 
-        :unit_name=>'access to cold emails seminar', 
+        :code=>'agency', 
+        :name=>'Positive Replies from Leads', 
+        :unit_name=>'warmlead', 
         :consumption=>BlackStack::I2P::CONSUMPTION_BY_TIME, 
         # formal description to show in the list of products
         :description=>'Liftetime access to our Cold Emails Seminar.',
@@ -91,8 +91,8 @@ BlackStack::I2P::add_services([
         :title=>'Learn how book meetings and close deals with cold emails, at an affordable cost.',
         # larger persuasive description to show in the sales letter
         :summary=>'This seminar teaches you how to get quality email databases at very low price, how deliver cold emails for free, and how to write cold emails in order to get responses.',
-        :thumbnail=>CS_HOME_WEBSITE+'/ces/images/logo.png',
-        :return_path=>CS_HOME_WEBSITE+'/ces/results',
+        :thumbnail=>CS_HOME_WEBSITE+'/agency/images/logo.png',
+        :return_path=>CS_HOME_WEBSITE+'/agency/dashboard',
         # what is the life time of this product or service?
         :credits_expiration_period => 'month',
         :credits_expiration_units => 1,
@@ -104,32 +104,7 @@ BlackStack::I2P::add_services([
             :units=>1,
         },
         # most popular plan configuratioon
-        :most_popular_plan => 'leads.batman',
+        :most_popular_plan => 'agency.batman',
     },
 ])
-
-# setup the I2P plans descriptors here
-BlackStack::I2P::add_plans([
-    {
-        # which product is this plan belonging
-        :service_code=>'ces', 
-        # recurrent billing plan or one-time payments
-        :type=>BlackStack::I2P::PAYMENT_PAY_AS_YOU_GO,  
-        # show this plan in the UI
-        :public=>false,
-        # is this a One-Time Offer?
-        # true: this plan is available only if the account has not any invoice using this plan
-        # false: this plan can be purchased many times
-        :one_time_offer=>true,  
-        # plan description
-        :item_number=>'ces.lifetime', 
-        :name=>'Cold Emails Seminar - Lifetime Access', 
-        # billing details
-        :credits=>1, 
-        :normal_fee=>97, # cognitive bias: expensive fee to show it strikethrough, as the normal price. But it's a lie. 
-        :fee=>27, # this is the fee that your will charge to the account, as a special offer price.
-        # Additional period after the billing cycle - Extend 2 weeks after the billing cycle - Referemce: https://github.com/ExpandedVenture/ConnectionSphere/issues/283.
-        :expiration_lead_period => 'year',
-        :expiration_lead_units => 1000,
-    }
-])
+=end
